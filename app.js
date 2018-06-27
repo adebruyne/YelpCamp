4a53035d914d338551d292725a9c892eca89ba44
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-27 13:50:24 -0400

    add express, body-parser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
 
 
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 //homepage
@@ -24,6 +25,11 @@ app.get("/campgrounds", function(req,res){
     res.render("campgrounds", {campgrounds:campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+    res.send("You hit the post route")
+    //get data from form and add to campgrounds array
+    //redirect back to campgrounds page
+})
 
 app.listen(8886, () => {
     console.log("The Yelpcamp server has started!")
