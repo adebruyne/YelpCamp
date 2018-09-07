6ade6ccd5bac251fc96664db5efec5b9d84a77f8
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-07 16:04:51 -0400

    added route to handle registration and authentication

--- app.js
+++ app.js
@@ -136,6 +136,20 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+//handle sign up logic
+app.post("/register", function(req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
 app.listen(8886, () => {
   console.log("The Yelpcamp server has started!");
 });
