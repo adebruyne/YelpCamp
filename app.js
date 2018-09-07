207dc3f8ca2839b08b7f408e0eabf776a65da2b0
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-07 15:57:53 -0400

    adding route for register form

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var seedDB = require("./seeds");
 var Comment = require("./models/comment");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
-var User = require('./models/user');
+var User = require("./models/user");
 
 //seed the database
 seedDB();
@@ -17,7 +17,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-//SCHEMA SETUP
+//PASSPORT CONFIGURTATION
+app.use(
+  require("express-session")({
+    secret: "Camping is the best!",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //homepage
 app.get("/", function(req, res) {
@@ -114,6 +126,14 @@ app.post("/campgrounds/:id/comments", function(req, res) {
       });
     }
   });
+});
+
+// =========
+//AUTH ROUTES
+
+//show register form
+app.get("/register", function(req, res) {
+  res.render("register");
 });
 
 app.listen(8886, () => {
