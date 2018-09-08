639fcbf03954184f918339cdc00bc7de45da58e7
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-08 14:34:46 -0400

    adding middleware to campgrounds routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -62,4 +62,12 @@ router.get("/campgrounds/:id", function(req, res) {
     });
 });
 
+//middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/login");
+  }
+
 module.exports = router;
