89f68e0ab2b4b20460e3f7e1f1e9fc118a7034ba
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-08 12:52:57 -0400

    add authentication to login route

--- app.js
+++ app.js
@@ -151,10 +151,19 @@ app.post("/register", function(req, res) {
 });
 
 //show login form
-app.get("/login", function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
-})
+});
 
+//handle login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
 
 app.listen(8886, () => {
   console.log("The Yelpcamp server has started!");
